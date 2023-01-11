a9135a63c8985257fb3570eb40b470f77b74ba6f
Oluwaseun770 <98650324+Oluwaseun770@users.noreply.github.com>
2023-01-11 17:41:27 -0400

    I added two scripts to the player
    I created a abilities script which makes the player choose certain abilities.
    and I created the inventory script which lets the player choose their weapon

---
+++ Overshade2dGame/Assets/Scripts/abilities.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class abilities : MonoBehaviour
+{
+    //this class contains all movement attributes of the player
+    //all attributes are  turned off by default
+    //attributes can be turned by accessing the object script
+    //EXTRA ATTRs CAN BE ADDED   
+    public bool canRun = false;
+    public bool canDash = false;
+    public bool canJump = false;
+    public bool canMove = false;
+    public bool canCrouch = false;
+
+    public float runSpeed = 0f;
+    public float dashSpeed = 0f;
+    public float jumpForce = 0f;
+    public float movementSpeed = 0f;
+    public float crouchSppeed = 0f;
+    
+}
